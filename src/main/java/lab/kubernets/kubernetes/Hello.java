9b3400e19e8a8d4d79b78ea150e54e1c71d32bfb
Jacaranda Perez <jacaranda.perez@gmail.com>
2021-09-16 14:41:18 +0200

    first commit

---
+++ src/main/java/lab/kubernets/kubernetes/Hello.java
@@ -0,0 +1,18 @@
+package lab.kubernets.kubernetes;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello world!";
+
+    }
+
+}
